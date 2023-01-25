4a9d424a2d1d5495c04a3649cc8367c83a559bc8
Dytsaw <tomennana.samuel@outlook.fr>
2023-01-25 12:11:30 +0100

    Page de connexion + condition getLastUsername

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login', methods: ['GET', 'POST'])]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('security/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+        ]);
+    }
+
+    #[Route('/deconnexion', 'app_logout')]
+    public function logout()
+    {
+    }
+}
