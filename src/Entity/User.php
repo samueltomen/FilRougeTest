1c5ba22001063d25d95f7c1e3b182ee70887ac1a
Dytsaw <tomennana.samuel@outlook.fr>
2023-01-30 16:09:48 +0100

    add () on email in User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -20,7 +20,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
-    #[Assert\Email]
+    #[Assert\Email()]
     private ?string $email = null;
 
     #[ORM\Column]
