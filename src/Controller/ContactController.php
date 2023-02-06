01c3d2a241ea9be035f4da20c51fc5fb548347df
Dytsaw <tomennana.samuel@outlook.fr>
2023-02-06 19:52:01 +0100

    bug mailTrap send email

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -12,7 +12,6 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
@@ -41,16 +40,13 @@ class ContactController extends AbstractController
             $email = (new Email())
                 ->from('hello@example.com')
                 ->to('you@example.com')
-                ->cc('cc@example.com')
-                //->bcc('bcc@example.com')
-                //->replyTo('fabien@example.com')
                 ->priority(Email::PRIORITY_HIGH)
                 ->subject('Time for Symfony Mailer!')
                 ->text('Sending emails is fun again!')
                 ->html(
                     '<p>See Twig integration for better HTML integration!</p>'
                 );
-                $mailer->send($email);
+            $mailer->send($email);
 
             $this->addFlash('success', 'Votre demande a bien été envoyer');
             return $this->redirectToRoute('app_contact');
